08a4e7e895e363777b0aa944fb0ee78a8043e070
jwoody02 <jowo4949@colorado.edu>
2023-11-28 13:30:06 -0700

    added helper functions for data manager

--- Sources/Shoppy/ShopData/ShopDataManager.swift
+++ Sources/Shoppy/ShopData/ShopDataManager.swift
@@ -12,7 +12,7 @@ public class ShopDataManager {
     public static let shared = ShopDataManager()
 
     private var collections: [CollectionViewModel] = []
-    private var productsByCollection: [String: [ProductViewModel]] = [:]
+    private var productsByCollectionId: [String: [ProductViewModel]] = [:]
     private var productCursorByCollection: [String: String?] = [:]
 
     private var collectionCursor: String?
@@ -34,7 +34,7 @@ public class ShopDataManager {
                 self.collectionCursor = collections.pageInfo.hasNextPage ? collections.items.last?.cursor : nil
                 // Initialize product arrays and cursors for each collection
                 collections.items.forEach {
-                    self.productsByCollection[$0.id] = []
+                    self.productsByCollectionId[$0.id] = []
                     self.productCursorByCollection[$0.id] = nil
                 }
             }
@@ -45,26 +45,38 @@ public class ShopDataManager {
 
     // Fetch products within a collection with pagination
     @discardableResult
-    public func fetchProducts(in collection: CollectionViewModel, limit: Int = 25, completion: @escaping (PageableArray<ProductViewModel>?) -> Void) -> Task? {
+    public func fetchProducts(in collection: CollectionViewModel, limit: Int = 25, completion: @escaping ([ProductViewModel]?) -> Void) -> Task? {
         let currentCursor = productCursorByCollection[collection.id] ?? nil
         return client?.fetchProducts(in: collection, limit: limit, after: currentCursor) { [weak self] result in
             guard let self = self else { return }
 
             if let products = result {
                 // Append products to the correct collection
-                self.productsByCollection[collection.id]?.append(contentsOf: products.items)
+                self.productsByCollectionId[collection.id]?.append(contentsOf: products.items)
                 // Update product cursor for this collection
                 self.productCursorByCollection[collection.id] = products.pageInfo.hasNextPage ? products.items.last?.cursor : nil
             }
             NotificationCenter.default.post(name: ShopDataManager.productsUpdatedNotification, object: nil, userInfo: ["collectionId": collection.id])
                 
-            completion(result)
+            completion(result?.items)
         }
     }
 
     // Function to get products for a specific collection
     public func products(in collection: CollectionViewModel) -> [ProductViewModel]? {
-        return productsByCollection[collection.id]
+        return productsByCollectionId[collection.id]
     }
     
+    // return the number of collections loaded
+    public func numberOfCollectionsLoaded() -> Int {
+        return self.collections.count
+    }
+    
+    // return optional collection at index
+    public func collectionAtIndex(index: Int) -> CollectionViewModel? {
+        if index > self.collections.count - 1 {
+            return nil
+        }
+        return self.collections[index]
+    }
 }
